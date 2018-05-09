14d9feb0bce20d266698a279a9b6c734d90b873c
gujjarir <gujjari.r@husky.neu.edu>
2018-05-08 20:11:29 -0400

    unit tested user story-2

--- EndToEndTest.java
+++ EndToEndTest.java
@@ -33,70 +33,75 @@ public class EndToEndTest {
         consoleShouldReceive(NO_OUTPUT);
     }
 
-//    @Ignore @Test public void sendAnEmail_AnotherExample_story1() {
-//    	Message.main("sally@example.com", "Greetings.\nHow's it going?");
-//        networkShouldReceive("connect smtp\n" +
-//                "To: sally@example.com\n" +
-//                "\n" +
-//                "Greetings.\n" +
-//                "How's it going?\n" +
-//                "\n" +
-//                "disconnect\n");
-//        consoleShouldReceive(NO_OUTPUT);
-//    }
-//
-//    @Ignore @Test public void showAnErrorAndDoNotSendIfTheEmailAddressIsInvalid_story2() {
-//    	Message.main("noatsign", "Hi there!");
-//        networkShouldReceive(NO_OUTPUT);
-//        consoleShouldReceive("Invalid email address: noatsign\n");
-//    }
-//
-//    @Ignore @Test public void showAnErrorAndDoNotSendIfTheBodyIsInvalid_story3() {
-//    	Message.main("dinah@example.com", "");
-//        networkShouldReceive(NO_OUTPUT);
-//        consoleShouldReceive("Cannot send an email with no body.\n");
-//    }
-//
-//    @Ignore @Test public void sendAnEmailToMultipleAddresses_story4() {
-//    	Message.main("sally@example.com,joe@example.com", "Hi everyone!");
-//        networkShouldReceive("connect smtp\n" +
-//                "To: sally@example.com\n" +
-//                "To: joe@example.com\n" +
-//                "\n" +
-//                "Hi everyone!\n" +
-//                "\n" +
-//                "disconnect\n");
-//        consoleShouldReceive(NO_OUTPUT);
-//    }
-//
-//    @Ignore @Test public void betterErrorHandlingsForMultipleAddresses_story5() {
-//    	Message.main("sallyatexample.com,joeatexample.com", "Hi everyone!");
-//        networkShouldReceive(NO_OUTPUT);
-//        consoleShouldReceive("Invalid email addresses: sallyatexample.com joeateasmple.com\n");
-//    }
-//
-//    @Ignore @Test public void sendAMessageInAnotherFormat_story6() {
-//    	Message.main("-im", "leslie@chat.example.com", ":-) hey there!");
-//        networkShouldReceive("connect chat\n" +
-//                "<leslie@chat.example.com>(:-) hey there!)\n" +
-//                "disconnect\n");
-//        consoleShouldReceive(NO_OUTPUT);
-//    }
-//
-//    @Ignore @Test public void chatsToMultipleAddressesGetSentIndividually_story7() {
-//    	Message.main("-im", "leslie@chat.example.com,joey@chat.example.com", "Hello.");
-//        networkShouldReceive("connect chat\n" +
-//                "<leslie@chat.example.com>(Hello.)\n" +
-//                "<joey@chat.example.com>(Hello.)\n" +
-//                "disconnect\n");
-//        consoleShouldReceive(NO_OUTPUT);
-//    }
-//
-//    @Ignore @Test public void handleErrorsGracefully_story8() {
-//    	Message.setNetwork(new BadNetworkConnection());
-//    	Message.main("joe@example.com", "Hi there!");
-//        consoleShouldReceive("Connection error. Please try again.\n");
-//    }
+    @Test public void sendAnEmail_AnotherExample_story1() {
+    	Message.main("sally@example.com", "Greetings.\nHow's it going?");
+    	network=(StringWriter) Message.getNetwork();
+    	console=(StringWriter)Message.getConsole();
+        networkShouldReceive("connect smtp\n" +
+                "To: sally@example.com\n" +
+                "\n" +
+                "Greetings.\n" +
+                "How's it going?\n" +
+                "\n" +
+                "disconnect\n");
+        consoleShouldReceive(NO_OUTPUT);
+        System.out.println();
+    }
+
+    @Test public void showAnErrorAndDoNotSendIfTheEmailAddressIsInvalid_story2() {
+    	Message.main("noatsign", "Hi there!");
+    	network=(StringWriter) Message.getNetwork();
+    	console=(StringWriter)Message.getConsole();
+        networkShouldReceive(NO_OUTPUT);
+        consoleShouldReceive("Invalid email address: noatsign\n");
+    }
+
+    @Ignore @Test public void showAnErrorAndDoNotSendIfTheBodyIsInvalid_story3() {
+    	Message.main("dinah@example.com", "");
+        networkShouldReceive(NO_OUTPUT);
+        consoleShouldReceive("Cannot send an email with no body.\n");
+    }
+
+    @Ignore @Test public void sendAnEmailToMultipleAddresses_story4() {
+    	Message.main("sally@example.com,joe@example.com", "Hi everyone!");
+        networkShouldReceive("connect smtp\n" +
+                "To: sally@example.com\n" +
+                "To: joe@example.com\n" +
+                "\n" +
+                "Hi everyone!\n" +
+                "\n" +
+                "disconnect\n");
+        consoleShouldReceive(NO_OUTPUT);
+    }
+
+    @Ignore @Test public void betterErrorHandlingsForMultipleAddresses_story5() {
+    	Message.main("sallyatexample.com,joeatexample.com", "Hi everyone!");
+        networkShouldReceive(NO_OUTPUT);
+        consoleShouldReceive("Invalid email addresses: sallyatexample.com joeateasmple.com\n");
+    }
+
+    @Ignore @Test public void sendAMessageInAnotherFormat_story6() {
+    	Message.main("-im", "leslie@chat.example.com", ":-) hey there!");
+        networkShouldReceive("connect chat\n" +
+                "<leslie@chat.example.com>(:-) hey there!)\n" +
+                "disconnect\n");
+        consoleShouldReceive(NO_OUTPUT);
+    }
+
+    @Ignore @Test public void chatsToMultipleAddressesGetSentIndividually_story7() {
+    	Message.main("-im", "leslie@chat.example.com,joey@chat.example.com", "Hello.");
+        networkShouldReceive("connect chat\n" +
+                "<leslie@chat.example.com>(Hello.)\n" +
+                "<joey@chat.example.com>(Hello.)\n" +
+                "disconnect\n");
+        consoleShouldReceive(NO_OUTPUT);
+    }
+
+    @Ignore @Test public void handleErrorsGracefully_story8() {
+    	Message.setNetwork(new BadNetworkConnection());
+    	Message.main("joe@example.com", "Hi there!");
+        consoleShouldReceive("Connection error. Please try again.\n");
+    }
 
     private void networkShouldReceive(String output) {
         assertThat(network.toString(), equalTo(output));
